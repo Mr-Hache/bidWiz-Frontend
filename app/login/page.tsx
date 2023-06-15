9a47f5d1c5069533bc6c3d3a2a0f82918d5c01b9
Nixbat <nixpat08@gmail.com>
2023-06-15 17:07:51 -0500

    navbar - login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,20 @@
+import style from "./login.module.css"
+
+
 export default function login() {
     return (
-    <div >
-        LOGIN
-    </div>
+        <form className={style.formSubmit}>
+            <div className={style.userName}>
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" />
+            </div>
+
+            <div className={style.userName}>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" />
+            </div>
+            <button className={style.login} type="submit">Login</button>
+        </form>
+ 
     )
 }
