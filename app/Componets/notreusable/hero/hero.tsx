4c52fa8ae7485cc071938f8ee86000b989299b31
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-14 22:13:31 -0300

    landing hero, ok. También agregué la carpeta SRC para cargar las imagenes del proyecto

--- app/Componets/notreusable/hero/hero.tsx
+++ app/Componets/notreusable/hero/hero.tsx
@@ -1,17 +1,30 @@
 import styles from "./hero.module.scss";
+import Image from "next/image";
+
+import landing1 from "../../../src/images/landing1.jpeg";
 
 export default function hero() {
   return (
     <section className={styles.hero}>
-      <div className={styles.conteinerText}>
-        <h1>Unlock Your Potential, Together!</h1>
-        <h3>
-          Ditch the Old Ways. Harness the Best Minds. Right Now. Right Here.
-        </h3>
-        <button className={styles.button1}>Find Wizard</button>
-        <button className={styles.button2}>Become Wizard</button>
+      <div className={styles.containerText}>
+        <div className={styles.text}>
+          <h1>Unlock Your Potential, Together!</h1>
+
+          <h4>Ditch the Old Ways. Harness the Best Minds.</h4>
+          <h4>Right Now. Right Here.</h4>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.button1}>Find Wizard</button>
+          <button className={styles.button2}>Become Wizard</button>
+        </div>
       </div>
-      <div className={styles.conteinerImg}></div>
+      <div className={styles.containerImg}>
+        <Image
+          src={landing1}
+          alt=""
+          style={{ width: "25vw", height: "20vw" }}
+        />
+      </div>
     </section>
   );
 }
