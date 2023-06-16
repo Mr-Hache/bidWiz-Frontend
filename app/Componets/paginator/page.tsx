8b8f10dcb594f303d55057a038014059b63fbe64
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-16 14:06:34 -0500

    agregación del componente paginator

---
+++ app/Componets/paginator/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React from 'react'
+import { useAppSelector, useAppDispatch } from "@/app/redux/hooks";
+import { setPage } from "@/app/redux/services/filtersSlice";
+
+
+
+export default function paginator() {
+    
+const dispatch = useAppDispatch();
+    const page = useAppSelector((state) => state.filters.page);
+
+    const handlePage = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.currentTarget.value === "Next" ? dispatch(setPage(page + 1)) : page==1?null:dispatch(setPage(page - 1)); 
+        
+    }
+
+
+
+    console.log(page)
+  return (
+     <div>
+        <button  onClick={handlePage}>Prev</button>
+        <span>{page}</span>
+        <button value="Next" onClick={handlePage}>Next</button>
+     </div> 
+  )
+  }
