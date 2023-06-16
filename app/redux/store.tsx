cd3c3089aafcbd21b74ff15d719cac9524ac6993
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-16 14:09:07 -0500

    traemos los estos filter y wizards al store

--- app/redux/store.tsx
+++ app/redux/store.tsx
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userApi } from "./services/userApi";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import filtersReducer from './services/filtersSlice'
+import wizardsReducer from './services/wizardsSlice'
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
+    filters: filtersReducer,
+    wizards: wizardsReducer
   },
   devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
