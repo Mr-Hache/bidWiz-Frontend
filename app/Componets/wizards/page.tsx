24b720f764e39fb9daa3ddcdebe94d901898e9a0
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-16 17:39:31 -0500

    agregando lógica de paginado, llega hasta el tamaño de wizards que existan

--- app/Componets/wizards/page.tsx
+++ app/Componets/wizards/page.tsx
@@ -5,27 +5,75 @@ import Paginator from '../paginator/page'
 import { useAppSelector, useAppDispatch } from "@/app/redux/hooks";
 
 import { setWizards } from "@/app/redux/services/wizardsSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 
 export default function wizards() {
- const dispatch = useAppDispatch();
-    const page = useAppSelector((state) => state.filters.page);
-    const wizards = useAppSelector((state) => state.wizards.wizards);
-    const { data } = useGetWizardsQuery({});
+  const size = 9;
+  const dispatch = useAppDispatch();
+  const page = useAppSelector((state) => state.filters.page);
+  const wizards = useAppSelector((state) => state.wizards.wizards);
+  const { data } = useGetWizardsQuery({});
+  const [statePage, setStatePage] = useState(true);
+  const [totalPage, setTotalPage] = useState(0);
 
-    useEffect(() => {
-        if (!wizards || wizards.length === 0) {
-        dispatch(setWizards(data));
+  useEffect(() => {
+    const allowPage = Math.ceil(totalPage / size);
+    if(page == allowPage) {
+      setStatePage(false);
+    }
+    else {
+      setStatePage(true);
+    }
+  }, [page, totalPage])
+
+
+  useEffect(() => {
+    fetch(`https://bidwiz-backend-production-db77.up.railway.app/users/wizards/count`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('ERROR Request');
         }
-    }, [wizards, dispatch, data]);
+        return response.json();
+      })
+      .then(data => {
+        setTotalPage(data);
+
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  })
+
+  useEffect(() => {
+    if (!wizards || wizards.length === 0) {
+      dispatch(setWizards(data));
+    }
+  }, [wizards, dispatch, data]);
 
 
-    return (
-        <div>
-            {wizards&&wizards.map((wizardUser: User) => {
-                return <Wizard key={wizardUser._id} wizardUser={wizardUser} />
-            })}
-            <Paginator />
-        </div>
-    )
+  useEffect(() => {
+    fetch(`https://bidwiz-backend-production-db77.up.railway.app/users/wizards?page=${page}&size=${size}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('ERROR Request');
+        }
+        return response.json();
+      })
+      .then(data => {
+        dispatch(setWizards(data));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [page]);
+
+  return (
+    <div>
+      {wizards && wizards.map((wizardUser: User) => {
+        return <Wizard key={wizardUser._id} wizardUser={wizardUser} />
+      })}
+      <Paginator statePage={statePage}/>
+    </div>
+  )
 }
