723510845b54a5f3bc7343c7cec4822d201dfe16
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-15 21:22:41 -0300

    ultimos cambios de sass

--- app/Componets/landingWizards/landingWizards.tsx
+++ app/Componets/landingWizards/landingWizards.tsx
@@ -5,27 +5,27 @@ import styles from "./landingWizards.module.scss";
 import { useGetWizardsQuery } from "@/app/redux/services/userApi";
 
 export default function landingWizards() {
- 
-const { data } = useGetWizardsQuery(null);
+  const { data } = useGetWizardsQuery({});
 
   return (
     <section className={styles.why}>
       <div className={styles.containerWhy}>
-       <div className="grid grid-cols-3">
-        {
-          data?.map((user) => (
+        <h1>ESTÁ FUNCIONANDO</h1>
+        <div className="grid grid-cols-3">
+          {data?.map((user) => (
             <div key={user._id}>
               <h1>{user.name}</h1>
-              <img src={user.image} alt="" width={300} height={300} />
+              <div className={styles.containerImg}>
+                <img src={user.image} alt="" width={200} height={200} />
+              </div>
               <div className={styles.text}>
-              <h4>{user.languages}</h4>
-              <h4>{user.subjects}</h4>
-              <h4>{user.experience.expJobs}</h4>
+                <h4>{user.languages}</h4>
+                <h4>{user.subjects}</h4>
+                <h4>{user.experience.expJobs}</h4>
               </div>
             </div>
-          ))
-        }
-      </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- app/redux/services/userApi.ts
+++ app/redux/services/userApi.ts
@@ -8,19 +8,41 @@ type User = {
   password: string;
   email: string;
   phoneNumber: string;
-  isWizard: boolean
-  languages: ['English', 'Spanish', 'Portuguese', 'German', 'French', 'Chinese', 'Japanese', 'Russian', 'Italian']
-  subjects: ["Mathematics", "Physics", "Chemistry", "Biology", "Economics", "Business Administration", "Accounting", "Computer Science", "Music Theory", "Political Science", "Law", "Programming"]
+  isWizard: boolean;
+  languages: [
+    "English",
+    "Spanish",
+    "Portuguese",
+    "German",
+    "French",
+    "Chinese",
+    "Japanese",
+    "Russian",
+    "Italian"
+  ];
+  subjects: [
+    "Mathematics",
+    "Physics",
+    "Chemistry",
+    "Biology",
+    "Economics",
+    "Business Administration",
+    "Accounting",
+    "Computer Science",
+    "Music Theory",
+    "Political Science",
+    "Law",
+    "Programming"
+  ];
   experience: {
-    title: string
-    origin: string
-    expJobs: number
-  }
-  image: string
-  isDisabled: boolean
-  role: ["admin", "user"]
+    title: string;
+    origin: string;
+    expJobs: number;
+  };
+  image: string;
+  isDisabled: boolean;
+  role: ["admin", "user"];
 };
-
 
 export const userApi = createApi({
   reducerPath: "userApi",
@@ -48,7 +70,10 @@ export const userApi = createApi({
         method: "DELETE",
       }),
     }),
-    updateUserPassword: builder.mutation<User, { username: string; password: string }>({
+    updateUserPassword: builder.mutation<
+      User,
+      { username: string; password: string }
+    >({
       query: ({ username, password }) => ({
         url: `users/${username}/password`,
         method: "PATCH",
@@ -61,18 +86,26 @@ export const userApi = createApi({
         method: "PATCH",
       }),
     }),
-    getWizards: builder.query<User[], {subjects?: string[], languages?: string[], page?: number, limit?: number}>({
-      query: ({subjects, languages, page = 1, limit = 10}) => {
+    getWizards: builder.query<
+      User[],
+      {
+        subjects?: string[];
+        languages?: string[];
+        page?: number;
+        limit?: number;
+      }
+    >({
+      query: ({ subjects, languages, page = 1, limit = 10 }) => {
         let url = `users/wizards?page=${page}&limit=${limit}`;
-    
-        if(subjects) {
+
+        if (subjects) {
           url += `&subjects=${subjects}`;
         }
-    
-        if(languages) {
+
+        if (languages) {
           url += `&languages=${languages}`;
         }
-    
+
         return url;
       },
     }),
