ee693c633c685be8b71d9693ea014aa0ca612e74
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-16 15:16:18 -0500

    trayendo al estado global los wizards, si no se han cargado aun desde el landing

--- app/Componets/wizards/page.tsx
+++ app/Componets/wizards/page.tsx
@@ -5,13 +5,19 @@ import Paginator from '../paginator/page'
 import { useAppSelector, useAppDispatch } from "@/app/redux/hooks";
 
 import { setWizards } from "@/app/redux/services/wizardsSlice";
-
+import { useEffect } from "react";
 
 export default function wizards() {
  const dispatch = useAppDispatch();
-const page = useAppSelector((state) => state.filters.page);
-const wizards = useAppSelector((state) => state.wizards.wizards);
+    const page = useAppSelector((state) => state.filters.page);
+    const wizards = useAppSelector((state) => state.wizards.wizards);
+    const { data } = useGetWizardsQuery({});
 
+    useEffect(() => {
+        if (!wizards || wizards.length === 0) {
+        dispatch(setWizards(data));
+        }
+    }, [wizards, dispatch, data]);
 
 
     return (
