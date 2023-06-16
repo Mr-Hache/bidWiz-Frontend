98431a02552edf563ca202a7eb3947c715607b8a
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-16 11:15:41 -0500

    renderizando las propiedades de los wizard en cada tarjeta

--- app/Componets/wizard/page.tsx
+++ app/Componets/wizard/page.tsx
@@ -1,9 +1,21 @@
+import { User } from "../../redux/services/userApi";
 
+interface WizardProps {
+    wizardUser: User;
+}
 
-export default function wizard() {
+const Wizard: React.FC<WizardProps> = ({ wizardUser }) => {
     return (
         <div>
-            <h2>componente Card</h2>
+            <img src={wizardUser.image} alt="" width={200} height={200} />
+            <h3>{wizardUser.name}</h3>
+            <h3>{wizardUser.lastName}</h3>
+            <h4>{wizardUser.experience.title}</h4>
+            <h4>{wizardUser.languages.join(', ')}</h4>
+            <h4>{wizardUser.subjects.join(', ')}</h4>
         </div>
-    )
+    );
 }
+
+export default Wizard;
+
