d6a671425d8c13532a99a9092ce708441a3a5104
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-17 15:05:53 -0500

    sustituyendo elemento img por Image de next

--- app/Componets/wizard/page.tsx
+++ app/Componets/wizard/page.tsx
@@ -1,26 +1,37 @@
 import { User } from "../../redux/services/userApi";
 import Link from 'next/link';
+import Image from 'next/image';
+
 interface WizardProps {
-    wizardUser: User;
+  wizardUser: User;
 }
 
 const Wizard: React.FC<WizardProps> = ({ wizardUser }) => {
-    
-
-    return (
-
+    const imageLoader = ({ src }: { src: string }) => {
+        return src; // Devuelve la ruta original de la imagen sin modificaciones
+      };
+  return (
     <div>
-        <Link href={`/detail/${wizardUser.username}`}>
-            <img src={wizardUser.image} alt="" width={200} height={200} />
-            <h3>{`${wizardUser.name} ${wizardUser.lastName}`}</h3>
-        </Link>
+      <Link href={`/detail/${wizardUser.username}`}>
         
-        <h4>{wizardUser.experience.title}</h4>
-        <h4>{wizardUser.languages.join(', ')}</h4>
-        <h4>{wizardUser.subjects.join(', ')}</h4>
+          <div>
+            <Image
+              loader={imageLoader}
+              src={wizardUser.image}
+              alt=""
+              width={200}
+              height={200}
+            />
+          </div>
+          <h3>{`${wizardUser.name} ${wizardUser.lastName}`}</h3>
+        
+      </Link>
+
+      <h4>{wizardUser.experience.title}</h4>
+      <h4>{wizardUser.languages.join(', ')}</h4>
+      <h4>{wizardUser.subjects.join(', ')}</h4>
     </div>
-    );
+  );
 }
 
 export default Wizard;
-
