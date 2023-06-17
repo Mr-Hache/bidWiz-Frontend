cc012f347685c025e1c2f6a29f82717cd9d4e709
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-16 23:38:33 -0500

    añadiendo lógica para conectar la searchbar

--- app/Componets/searchBar/searchBar.tsx
+++ app/Componets/searchBar/searchBar.tsx
@@ -1,14 +1,32 @@
 import styles from "./searchBar.module.scss";
+import { useGetUserByUsernameQuery } from '@/app/redux/services/userApi';
+import {useState} from 'react'
 
 export default function searchBar() {
+  
+  const [search, setSearch] = useState<string>('')
+  
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value)
+  }
+  
+    const { data: user, isLoading, isError } = useGetUserByUsernameQuery({username: search});
+    console.log(user);
+    
+  
+
+
   return (
     <div className={styles.searchBar}>
       <input
         className={styles.input}
         type="text"
         placeholder="Find your Wizards"
+        value={search}
+        onChange={handleSearch}
       />
-      <button className={styles.button} type="submit">
+      <button  className={styles.button} type="submit">
         <div className={styles.lupa}>🔍︎</div>
       </button>
     </div>
