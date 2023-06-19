e74f6371a680d8e85ccf9d220df6431b2fb56e75
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-18 21:24:44 -0300

    Cambié el renderizado de la navBar que estaba en el archivo layout, y lo puse en cada una de las rutas

--- app/detail/[username]/page.tsx
+++ app/detail/[username]/page.tsx
@@ -1,39 +1,39 @@
+"use client";
 
-"use client"
-
-import { usePathname } from 'next/navigation';
-import { useGetUserByUsernameQuery } from '@/app/redux/services/userApi';
-
+import { usePathname } from "next/navigation";
+import { useGetUserByUsernameQuery } from "@/app/redux/services/userApi";
+import Navbar from "@/app/Componets/navbar/navbar";
 
 function detail() {
   const pathname = usePathname(); // imprime la ruta actual
-  const username = pathname.split('/')[2];
-  
-  const { data: user, isLoading, isError } = useGetUserByUsernameQuery({username});
+  const username = pathname.split("/")[2];
+
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useGetUserByUsernameQuery({ username });
   if (isLoading) return <div>Loading...</div>;
   if (isError || !user) return <div>User not found</div>;
 
   return (
     <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <h1>{user.username}</h1>
-        <img src={user.image} alt="" width={300} height={300} />
-        <h2>{`${user.name} ${user.lastName}`}</h2>
-        <h2>{user.experience.title}</h2>
-        <h2>{user.experience.origin}</h2>
-        <h2>{user.experience.expJobs}</h2>
-        <h3>{user.languages.join(', ')}</h3>
-        <h3>{user.subjects.join(', ')}</h3>
+      <Navbar />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>{user.username}</h1>
+      <img src={user.image} alt="" width={300} height={300} />
+      <h2>{`${user.name} ${user.lastName}`}</h2>
+      <h2>{user.experience.title}</h2>
+      <h2>{user.experience.origin}</h2>
+      <h2>{user.experience.expJobs}</h2>
+      <h3>{user.languages.join(", ")}</h3>
+      <h3>{user.subjects.join(", ")}</h3>
     </div>
   );
 }
 
 export default detail;
-
-
-
-
-
