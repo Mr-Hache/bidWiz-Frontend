dbbd26f79bb3f59b6fa91a0e5d32bc7c86d7072d
mickelsengerald <mickelsengerald@gmail.com>
2023-06-19 20:00:19 -0400

    cambio d elogica en componente auth para estilos

--- app/Componets/auth/page.tsx
+++ app/Componets/auth/page.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import style from "./auth.module.scss"
+import  Link  from 'next/link'
 
 function auth() {
     return (
     
         <form className={style.form}>
             <div  className={style.container}>
-                <label htmlFor="username" className={style.label}>Username</label><br/>
+                <label htmlFor="username" className={style.label}>Username</label><br />
                 <input type="text" name="username"  className={style.input}/>
             </div>
+            <br />
             <div className={style.container}>
-                <label htmlFor="password">Password</label><br/>
+                <label htmlFor="password" className={style.label}>Password</label><br />
                 <input type="password" name="password" className={style.input}/>
             </div>
-            <button type="submit" className={style.button}>Login</button>
+            <Link href="/not-found" className={style.link}>  
+                <button type="submit" className={style.button}>Login</button>
+            </Link>
+            
         </form>
         
   )
