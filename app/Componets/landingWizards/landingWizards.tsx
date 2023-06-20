8c4aee3d7fa5aefbfc6a62d447e7bde9b1ebfc70
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-20 12:43:12 -0300

    Agregamos un efecto a las tarjetas y también el link para que lleve al detail de cada uno de los wizards renderizados

--- app/Componets/landingWizards/landingWizards.tsx
+++ app/Componets/landingWizards/landingWizards.tsx
@@ -3,45 +3,44 @@
 import styles from "./landingWizards.module.scss";
 import Link from "next/link";
 
-import { setWizards } from "@/app/redux/services/wizardsSlice"
+import { setWizards } from "@/app/redux/services/wizardsSlice";
 import { useGetWizardsQuery } from "@/app/redux/services/userApi";
 import { useAppDispatch } from "@/app/redux/hooks";
 
-
-
 export default function landingWizards() {
   const dispatch = useAppDispatch();
-
   const { data } = useGetWizardsQuery({});
 
-
-  dispatch(setWizards(data))
-
-
-
-
-
+  dispatch(setWizards(data));
 
   return (
     <section className={styles.wizards}>
       <div className={styles.title}>
-        <h1>Want to learn from the best in bidWiz?</h1>
+        <h1>Want to learn from the best in BidWiz?</h1>
       </div>
       <div>
         <div className={styles.containerW}>
-          {data?.slice(0, 5).map((user) => (
+          {data?.slice(0, 6).map((user) => (
             <div className={styles.cardw} key={user._id}>
-              <div className={styles.containerImg}>
-                <img src={user.image} alt="" width={200} height={200} />
-              </div>
-              <div className={styles.text}>
-                <h3>{`${user.name} ${user.lastName}`}</h3>
-                <h4>
-                  Gives {user.subjects.toString().split(/\W+/)[0]} classes
-                </h4>
-                <h4>in {user.languages.toString().split(/\W+/)[0]}</h4>
-                <h4 className={styles.online}>Online</h4>
-              </div>
+              <Link
+                style={{ textDecoration: "none" }}
+                href={`/detail/${user.username}`}
+              >
+                <div className={styles.containerImg}>
+                  <img src={user.image} alt="" width={200} height={200} />
+                </div>
+                <div className={styles.text}>
+                  <h3>{`${user.name} ${user.lastName}`}</h3>
+                  <h4>
+                    Gives {user.subjects.toString().split(/\W+/)[0]} classes
+                  </h4>
+                  <h4>
+                    in {user.languages.toString().split(/\W+/)[1]} or{" "}
+                    {user.languages.toString().split(/\W+/)[0]}
+                  </h4>
+                  <h4 className={styles.online}>Online</h4>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
