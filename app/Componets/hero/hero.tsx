e95c41e052ee0c8513f6ce4555e046753e8559c0
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-20 10:59:33 -0300

    Cambios en la foto del Hero de la landing page. Esto está para evaluar, no es definitivo

--- app/Componets/hero/hero.tsx
+++ app/Componets/hero/hero.tsx
@@ -2,37 +2,39 @@ import styles from "./hero.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
-import landing1 from "../../src/images/landing/landing1.jpeg";
+import landing1 from "../../src/images/landing/landing2.png";
 
 export default function hero() {
   return (
     <section className={styles.hero}>
-      <div className={styles.containerText}>
-        <div className={styles.text}>
-          <h1>Unlock Your Potential, Together!</h1>
+      <div className={styles.container}>
+        <div className={styles.containerText}>
+          <div className={styles.text}>
+            <h1>Unlock Your Potential, Together!</h1>
 
-          <h4>Ditch the Old Ways. Harness the Best Minds.</h4>
-          <h4>Right Now. Right Here.</h4>
+            <h4>Ditch the Old Ways. Harness the Best Minds.</h4>
+            <h4>Right Now. Right Here.</h4>
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button1}>
+              <Link href="/offerBoard" passHref>
+                Find Wizard
+              </Link>
+            </button>
+            <button className={styles.button2}>
+              <Link href="/register" passHref>
+                Become Wizard
+              </Link>
+            </button>
+          </div>
         </div>
-        <div className={styles.buttons}>
-          <button className={styles.button1}>
-            <Link href="/offerBoard" passHref>
-              Find Wizard
-            </Link>
-          </button>
-          <button className={styles.button2}>
-            <Link href="/register" passHref>
-              Become Wizard
-            </Link>
-          </button>
+        <div className={styles.containerImg}>
+          <Image
+            src={landing1}
+            alt=""
+            style={{ width: "23vw", height: "35vw" }}
+          />
         </div>
-      </div>
-      <div className={styles.containerImg}>
-        <Image
-          src={landing1}
-          alt=""
-          style={{ width: "25vw", height: "20vw" }}
-        />
       </div>
     </section>
   );
