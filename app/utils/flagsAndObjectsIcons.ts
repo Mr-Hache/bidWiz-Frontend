7497a3478f6a05e04ed9ff96c94db13b19c51a84
Nixbat <nixpat08@gmail.com>
2023-06-21 11:53:19 -0500

    iconos flags

---
+++ app/utils/flagsAndObjectsIcons.ts
@@ -0,0 +1,92 @@
+import React from 'react';
+
+
+export const flags = [
+    {
+      name: "English",
+      flag: "GB",
+    },
+    {
+      name: "Chinese",
+      flag: "CN",
+    },
+    {
+      name: "French",
+      flag: "FR",
+    },
+    {
+      name: "German",
+      flag: "DE",
+    },
+    {
+      name: "Italian",
+      flag: "IT",
+    },
+    {
+      name: "Japanese",
+      flag: "JP",
+    },
+    {
+      name: "Portuguese",
+      flag: "PT",
+    },
+    {
+      name: "Russian",
+      flag: "RU",
+    },
+    {
+      name: "Spanish",
+      flag: "ES",
+    },
+  ];
+
+  export const subjectsIcons = [
+    {
+      name: "Accounting",
+      icon: "FaBook",
+    },
+    {
+      name: "Biology",
+      icon: "FaMicroscope",
+    },
+    {
+      name: "Business Administration",
+      icon: "FaBriefcase",
+    },
+    {
+      name: "Chemistry",
+      icon: "FaVial",
+    },
+    {
+      name: "Computer Science",
+      icon: "FaCode",
+    },
+    {
+      name: "Economics",
+      icon: "FaRegChartBar",
+    },
+    {
+      name: "Law",
+      icon: "FaBalanceScale",
+    },
+    {
+      name: "Mathematics",
+      icon: "FaCalculator",
+    },
+    {
+      name: "Music Theory",
+      icon: "FaMusic",
+    },
+    {
+      name: "Physics",
+      icon: "FaAtom",
+    },
+    {
+      name: "Political Science",
+      icon: "FaUserGraduate",
+    },
+    {
+      name: "Programming",
+      icon: "FaLaptopCode"
+    }
+  ];
