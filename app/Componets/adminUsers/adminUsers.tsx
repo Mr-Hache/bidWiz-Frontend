608188e80a2b5e0625f98f7097287afb422d7b97
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-22 19:19:47 -0300

    agregué el componente adminUsers que va a renderizar las informaciones de los usuarios en el dashboard del admin

---
+++ app/Componets/adminUsers/adminUsers.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./adminUsers.module.scss";
+
+export default function adminUsers() {
+  return (
+    <div className={styles.containUsers}>
+      <h1>Users</h1>
+    </div>
+  );
+}
