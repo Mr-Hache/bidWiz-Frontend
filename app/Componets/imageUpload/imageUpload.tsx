44c8c91fe9dcb4dabd1e57118a1b92bf3c209b8b
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-21 22:11:28 -0300

    La foto quedó en formato obligatorio en el registro de wizards
    Los ultimos dos commits están intercambiados

--- app/Componets/imageUpload/imageUpload.tsx
+++ app/Componets/imageUpload/imageUpload.tsx
@@ -16,38 +16,36 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onImageUpload }) => {
   ) => {
     const file = event.target.files && event.target.files[0];
     if (file) {
-      try {
-        const formData = new FormData();
-        formData.append("file", file);
-        formData.append("upload_preset", "bidwiz"); // Reemplaza con tu upload preset de Cloudinary
+      if (file.size <= 2 * 1024 * 1024) {
+        // Restricción de tamaño a 2MB
+        try {
+          const formData = new FormData();
+          formData.append("file", file);
 
-        const response = await fetch(
-          `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-          {
-            method: "POST",
-            body: formData,
+          formData.append("upload_preset", "bidwiz"); // Reemplaza con tu upload preset de Cloudinary
+
+          const response = await fetch(
+            `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+            {
+              method: "POST",
+              body: formData,
+            }
+          );
+          if (response.ok) {
+          } else {
           }
-        );
-        if (response.ok) {
-          console.log("todo correcto");
-        } else {
-          console.log("error");
-        }
 
-        const data = await response.json();
-        setImage(data.secure_url);
-        onImageUpload(data.secure_url);
-
-        console.log(image);
-        console.log(data);
-      } catch (error) {
-        console.error("Error uploading image to Cloudinary:", error);
+          const data = await response.json();
+          setImage(data.secure_url);
+          onImageUpload(data.secure_url);
+        } catch (error) {}
       }
     }
   };
 
   return (
     <div>
+      <label>Select Avatar</label>
       <input
         type="file"
         accept="image/*"
@@ -55,7 +53,6 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onImageUpload }) => {
         onChange={handleImageChange}
       />
       {image && <img src={image} alt="Uploaded" width="200px" />}{" "}
-      {/* Mostrar la imagen cargada */}
     </div>
   );
 };
