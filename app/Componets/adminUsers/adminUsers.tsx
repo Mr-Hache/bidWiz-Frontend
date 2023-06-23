ab31048f66120f257654b2c6f25b7ecefc67ad1a
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-22 22:40:30 -0300

    agregué el grafico de los usuarios, falta colocar la logica

--- app/Componets/adminUsers/adminUsers.tsx
+++ app/Componets/adminUsers/adminUsers.tsx
@@ -1,10 +1,56 @@
+"use client";
+
 import React from "react";
-import styles from "./adminUsers.module.scss";
+import ApexCharts from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+import { ChartOptions } from "../adminSales/adminSales";
 
 export default function adminUsers() {
+  const options: ChartOptions = {
+    chart: {
+      height: 350,
+      type: "bar",
+      zoom: {
+        enabled: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "55%",
+        borderRadius: 10,
+      },
+    },
+    stroke: {
+      show: false,
+      width: 1,
+      colors: ["transparent"],
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: ["Alumnos", "Wizards", "Total"],
+    },
+    labels: ["Alumnos", "Wizards", "Total"],
+  };
+
+  const series = [
+    {
+      data: [80, 35, 115],
+    },
+  ];
+
   return (
-    <div className={styles.containUsers}>
+    <div>
       <h1>Users</h1>
+      <ApexCharts
+        options={options}
+        series={series}
+        type="bar"
+        width={400}
+        height={300}
+      />
     </div>
   );
 }
