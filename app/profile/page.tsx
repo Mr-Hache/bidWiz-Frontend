8519acb8fad9bbe769c88694b4fb59895ca375fa
mickelsengerald <mickelsengerald@gmail.com>
2023-06-23 10:16:59 -0400

    actualice los hooks con los de job y cre el comp editprofile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,17 @@
 import Navbar from "@/app/Componets/navbar/navbar";
+import EditProfile from "../Componets/editProfile/page";
 
 export default async function profile(){
 
     return (
         <div>
             <Navbar/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <EditProfile/>
         </div>
     )
 }
