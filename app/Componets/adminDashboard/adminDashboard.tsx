75835bba41f4a67d5cec4564831049b935cc7553
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-23 18:06:33 -0300

    Renderizamos las tablas en el dashboard

--- app/Componets/adminDashboard/adminDashboard.tsx
+++ app/Componets/adminDashboard/adminDashboard.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import React from "react";
 import styles from "./adminDashboard.module.scss";
 import AdminUsers from "../adminUsers/adminUsers";
 import AdminSales from "../adminSales/adminSales";
 import AdminDistribution from "../adminDistribution/adminDistribution";
+import AdminTableSeller from "../adminTableSellers/adminTableSeller";
+import AdminTableBuyers from "../adminTableBuyers/adminTableBuyers";
+import AdminDisabled from "../adminDisabled/adminDisabled";
+import AdminTableEarners from "../adminTableEarners/adminTableEarners";
 
 export default function adminDashboard() {
   return (
@@ -11,6 +17,14 @@ export default function adminDashboard() {
       <div className={styles.container}>
         <AdminSales />
         <AdminDistribution />
+      </div>
+      <div className={styles.container}>
+        <AdminTableSeller />
+        <AdminTableEarners />
+        <AdminTableBuyers />
+      </div>
+      <div>
+        <AdminDisabled />
       </div>
     </div>
   );
