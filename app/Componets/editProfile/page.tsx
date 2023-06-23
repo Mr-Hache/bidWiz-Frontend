147921aa0804c0dde7de0279b0e79966830886c9
mickelsengerald <mickelsengerald@gmail.com>
2023-06-23 10:30:44 -0400

    despliegue basico de upsate wizard en el editprofile

--- app/Componets/editProfile/page.tsx
+++ app/Componets/editProfile/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client"
 
-function editProfile() {
+import React, { useState } from 'react'
+import { useUpdateWizardStatusMutation, UpdateUserWizardDto } from '../../redux/services/userApi'
+
+function EditProfile() {
+  const [updateWizardStatus, { isLoading }] = useUpdateWizardStatusMutation();
+  const [formState, setFormState] = useState<UpdateUserWizardDto>({
+    // Inicializa con valores por defecto o con los valores actuales del usuario
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await updateWizardStatus({
+        _id: 'userId', // Actualiza con el ID real del usuario
+        updateUserWizardDto: formState,
+      });
+      console.log(response);
+      // Realiza algo con la respuesta, como redirigir o mostrar un mensaje de éxito.
+    } catch (error) {
+      console.error(error);
+      // Maneja el error aquí
+    }
+  };
+
   return (
-    <div>editProfile</div>
-  )
+    <form onSubmit={handleSubmit}>
+      {/* Aquí deberías tener los inputs correspondientes para cada propiedad que quieras actualizar, por ejemplo: */}
+      <input
+        type="text"
+        name="aboutMe"
+        value={formState.aboutMe || ''}
+        onChange={handleChange}
+      />
+      <button type="submit" disabled={isLoading}>Actualizar</button>
+    </form>
+  );
 }
 
-export default editProfile
+export default EditProfile;
