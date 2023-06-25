61976dc8cb142683f59b612afdba95beb2bf9b46
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-25 19:35:00 -0300

    agregué los estilos de las tablas donde se renderizan los que mas facturan, mas venden y mas compran

--- app/Componets/adminTableBuyers/adminTableBuyers.tsx
+++ app/Componets/adminTableBuyers/adminTableBuyers.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useEffect, useState } from "react";
+import styles from "../adminTableEarners/adminTableEarners.module.scss";
 
 export interface UserFormValues {
   totalPaid: number;
@@ -25,23 +26,28 @@ export default function adminTableBuyer() {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Best Buyers</h1>
-      <div>
-        <ul>
-          {buyerData.slice(0, 5).map((buyer) => (
-            <li>
+      <div className={styles.containerTable}>
+        {buyerData.slice(0, 5).map((buyer) => (
+          <div className={styles.tableContents}>
+            <div className={styles.image}>
               <img
+                className={styles.image}
                 src={buyer.user_info.image}
                 alt={""}
                 width="50"
                 height="50"
               />
+            </div>
+            <div className={styles.text}>
               <p>{buyer.user_info.name}</p>
-              <p>U$D {buyer.totalPaid}</p>
-            </li>
-          ))}
-        </ul>
+              <p>
+                <b>U$D {buyer.totalPaid}</b>
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
