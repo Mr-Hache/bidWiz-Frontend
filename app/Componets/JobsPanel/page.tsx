19d991ac9e1a0ed126cc4707af4a52e9f9142634
Francisco_I <francisco.insaurralde21@gmail.com>
2023-06-25 19:20:39 -0300

    jobs panel ahora funciona con cuetas logeadas y estilos basicos aplicados

--- app/Componets/JobsPanel/page.tsx
+++ app/Componets/JobsPanel/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 
+import styles from "./jobsPanel.module.scss";
 import { useGetJobsByWorkerQuery, useGetJobsByClientQuery, useUpdateJobWorkerMutation, useUpdateJobReviewMutation } from "@/app/redux/services/userApi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {useAppSelector} from "../../redux/hooks"
+
 
 interface JobReview {
   jobId: string;
@@ -11,30 +14,51 @@ interface JobReview {
 
 export default function JobsPanel() {
 
-  
-  const clientId = "648e17f0c8489b5c103ee650"
-  const workerId = "648e1817c8489b5c103ee67a"
+  const [jobToChange, setJobToChange] = useState({
+    jobId: "",
+    workerId: "",
+    updateJobWorkerDto: {status: 'Finished'}, //'In Progress' | 'Finished'
+  });
+  const [jobReview, setJobReview] = useState({
+    jobId: "",
+    clientId: "",
+    updateJobReviewDto: {rating: 1}, //DEL 1 AL 5
+  });
 
-  const getJobsByWorkerQuery = useGetJobsByWorkerQuery({ workerId });
+  const localUid = useAppSelector((state) => state.userAuth.uid)
+  // const clientId = "648e17f0c8489b5c103ee650"
+  // const workerId = "648e1817c8489b5c103ee67a"
+  const [userId, setUserId] = useState('');
+  const fetchUserData = async () => {
+    await fetch(`https://bidwiz-backend-production-db77.up.railway.app/users/user/${localUid}`)
+    .then(response => response.json())
+    .then(data => {
+    setUserId(data._id)
+})
+    .catch(error => console.error(error));
+}
+useEffect( () => {
+  fetchUserData(); 
+  setJobReview(() => ({
+    ...jobReview,
+    clientId: userId,
+  }));
+  setJobToChange(() => ({
+    ...jobToChange,
+    workerId: userId ,
+  }));
+}, [userId]);
+  const getJobsByWorkerQuery = useGetJobsByWorkerQuery({ workerId:userId });
 
-  const getJobsByClientQuery = useGetJobsByClientQuery({ clientId });
+  const getJobsByClientQuery = useGetJobsByClientQuery({ clientId:userId });
+
 
   const [updateJobWorkerMutation] = useUpdateJobWorkerMutation();
 
   const [updateJobReviewMutation] = useUpdateJobReviewMutation();
 
-  const [jobToChange, setJobToChange] = useState({
-    jobId: "",
-    workerId: "648e1817c8489b5c103ee67a",
-    updateJobWorkerDto: {status: 'Finished'}, //'In Progress' | 'Finished'
-  });
-  const [jobReview, setJobReview] = useState({
-    jobId: "",
-    clientId: "648e17f0c8489b5c103ee650",
-    updateJobReviewDto: {rating: 1}, //DEL 1 AL 5
-  });
+  console.log(userId)
 
-  // Acceder a los resultados de la consulta
   const { data: jobsByWorker, isLoading: isLoadingJobsByWorker, isError: isErrorJobsByWorker } = getJobsByWorkerQuery;
   const { data: jobsByClient, isLoading: isLoadingJobsByClient, isError: isErrorJobsByClient } = getJobsByClientQuery;
 
@@ -46,6 +70,7 @@ export default function JobsPanel() {
       updateJobReviewDto: {rating:value} ,
     }));
     console.log(jobReview)
+    
   };
   const onChangeReviewJobId = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value: string = event.target.value;
@@ -53,6 +78,7 @@ export default function JobsPanel() {
       ...jobReview,
       jobId: value ,
     }));
+    setClientInReviewViews(() => jobsByClient?.find((objeto: any) => objeto._id === value));
   };
   const onClickHandlerReview = async () => {
     console.log(jobReview)
@@ -65,12 +91,14 @@ export default function JobsPanel() {
   ////////////////////////////////////////////////////////////
 
   const onChangeStatusJobId = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    
     const value: string = event.target.value;
     console.log(jobToChange)
     setJobToChange(() => ({
       ...jobToChange,
       jobId: value ,
     }));
+    setInProgessJobViews(() => jobsByWorker?.find((objeto: any) => objeto._id === value));
   };
   const onClickHandlerJobStatus = async () => {
     console.log(jobToChange)
@@ -80,39 +108,121 @@ export default function JobsPanel() {
       console.error(error);
     }
   }
+  const onChangeStatusJobIdFinished = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value: string = event.target.value;
+    setCompletedJobViews(() => jobsByWorker?.find((objeto: any) => objeto._id === value));
+  };
+
+  const onChangeInProgressClient = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value: string = event.target.value;
+    setClientInProgressViews(() => jobsByClient?.find((objeto: any) => objeto._id === value));
+  };
+  /////////////////////////////////////////////////
+  const [InProgessJobViews, setInProgessJobViews] = useState({
+    
+  })
+  const [completedJobViews, setCompletedJobViews] = useState({
+
+  })
+  const [clientInProgressViews, setClientInProgressViews] = useState({
+
+  })
+  const [clientInReviewViews, setClientInReviewViews] = useState({
+
+  })
     return (
-      <div>
-        WORKER/// WORK IN PROGRESS:
-        <select name="" onChange={onChangeStatusJobId}>
-          {jobsByWorker?.map((job)  => {if (job.status ==='In Progress') {return(<option value={job._id}>{job.description}</option>)}}  )}
-        </select>
-        <button onClick={onClickHandlerJobStatus}>Complete Job</button>
-        <select name="">
-          {jobsByWorker?.map((job)  => {if (job.status ==="Finished") {return(<option value={job._id}>{job.description}</option>)}}  )}
-        </select>
-        <br></br>
-        CLIENT/// jobs in progress:
-        <select name="">
-          {jobsByClient?.map((job)  => {if (job.status ==='In Progress') {return(<option value={job._id}>{job.description}</option>)}}  )}
-        </select>
-        <br></br>
-        CLIENT/// jobs to review:
-        <select name="" onChange={onChangeReviewJobId}>
-          {jobsByClient?.map((job)  => {if (job.status ==="Finished") {return(<option value={job._id}>{job.description}</option>)}}  )}
-        </select>
-        CLIENT/// Work Rating:
-        <select name="" onChange={onChangeReview}>
-        <option value="1">Select Points</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
+<div className={styles.div}>
+      <h1>In Progress jobs:</h1>
+      <select name="" onChange={onChangeStatusJobId}>
+        <option>Select</option>
+        {jobsByWorker?.map((job) => {
+          if (job.status === 'In Progress') {
+            return <option value={job._id}>{job.description}</option>;
+          }
+        })}
       </select>
-        <button onClick={onClickHandlerReview}>give review</button>
+      {InProgessJobViews && (
+        <div className={styles.paneljobs}>
+          <div>Language: {InProgessJobViews.language}</div>
+          <div>NumClasses: {InProgessJobViews.numClasses}</div>
+          <div >Price: {InProgessJobViews.price}</div>
+          <div >Status: {InProgessJobViews.status}</div>
+          <div >Subject: {InProgessJobViews.subject}</div>
+        </div>
+      )}
+      <button  onClick={onClickHandlerJobStatus}>Complete Job</button>
+
+      <h1>Completed:</h1>
+      <select  name="" onChange={onChangeStatusJobIdFinished}>
+        <option>Select</option>
+        {jobsByWorker?.map((job) => {
+          if (job.status === "Finished") {
+            return <option value={job._id}>{job.description}</option>;
+          }
+        })}
+      </select>
+      <div>
+        {completedJobViews && (
+          <div className={styles.paneljobs}>
+            <div >Language: {completedJobViews.language}</div>
+            <div >NumClasses: {completedJobViews.numClasses}</div>
+            <div >Price: {completedJobViews.price}</div>
+            <div >Status: {completedJobViews.status}</div>
+            <div >Subject: {completedJobViews.subject}</div>
+          </div>
+        )}
       </div>
+  <h1>Jobs in progress:</h1>
+  <select name="" onChange={onChangeInProgressClient}>
+    <option>Select</option>
+    {jobsByClient?.map((job) => {
+      if (job.status === 'In Progress') {
+        return <option value={job._id}>{job.description}</option>;
+      }
+    })}
+  </select>
+  {clientInProgressViews && (
+    <div className={styles.paneljobs}>
+      <div >Language: {clientInProgressViews.language}</div>
+      <div >NumClasses: {clientInProgressViews.numClasses}</div>
+      <div >Price: {clientInProgressViews.price}</div>
+      <div >Status: {clientInProgressViews.status}</div>
+      <div >Subject: {clientInProgressViews.subject}</div>
+    </div>
+  )}
+  <h1>Jobs to review:</h1>
+  <select name="" onChange={onChangeReviewJobId}>
+    <option>Select</option>
+    {jobsByClient?.map((job) => {
+      if (job.status === "Finished") {
+        return <option value={job._id}>{job.description}</option>;
+      }
+    })}
+  </select>
+  <h2>Work Rating:</h2>
+  <select name="" onChange={onChangeReview}>
+    <option value="1">Select Points</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+  </select>
+  {clientInReviewViews && (
+    <div className={styles.paneljobs}>
+      <div >Language: {clientInReviewViews.language}</div>
+      <div >NumClasses: {clientInReviewViews.numClasses}</div>
+      <div >Price: {clientInReviewViews.price}</div>
+      <div >Status: {clientInReviewViews.status}</div>
+      <div >Subject: {clientInReviewViews.subject}</div>
+    </div>
+  )}
+  <button onClick={onClickHandlerReview}>Give Review</button>
+</div>
+
     );
   }
 
 
+  
 
