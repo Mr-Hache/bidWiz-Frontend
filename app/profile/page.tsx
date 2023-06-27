d3dc6e185b02b95033ed428a455cc17205ec6d84
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-27 20:13:44 -0300

    Quité el titulo de la pagina, cree el module.scss y le puse un margin top y margin botton para que se separe de arriba y de abajo

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,26 +4,24 @@ import EditProfile from "../Componets/editProfile/page";
 import UnableAccount from "../Componets/UnableAccount/page";
 import JobsPanel from "../Componets/JobsPanel/page";
 import Accordion from "../Componets/accordion/accordion";
+import styles from "./profile.module.scss";
 
 export default async function profile() {
   return (
     <div>
       <Navbar />
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <h1>My Profile</h1>
-      <Accordion title="Edit Profile">
-        <EditProfile />
-      </Accordion>
-      <Accordion title="Jobs Panel">
-        <JobsPanel />
-      </Accordion>
-      <Accordion title="Unable Account">
-        <UnableAccount />
-      </Accordion>
+      <div className={styles.containerAccordion}>
+        <Accordion title="Edit Profile">
+          <EditProfile />
+        </Accordion>
+        <Accordion title="Jobs Panel">
+          <JobsPanel />
+        </Accordion>
+        <Accordion title="Management Account">
+          <UnableAccount />
+        </Accordion>
+      </div>
     </div>
   );
 }
