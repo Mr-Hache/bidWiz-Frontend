f0c2635cd1e04c6af56b8bc9971802318fdab820
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-28 14:18:12 -0500

    ahora el componente profile me renderiza solo el componente de protección de la ruta de profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,22 +1,11 @@
-import Navbar from "@/app/Componets/navbar/navbar";
-import EditProfile from "../Componets/editProfile/page";
+import ProtectionProfile from "../Componets/ProtectionProfile/ProtectionProfile";
 
-import UnableAccount from "../Componets/UnableAccount/page";
-import JobsPanel from "../Componets/JobsPanel/page";
+
 
 export default async function profile(){
-
-    return (
+    return(
         <div>
-            <Navbar/>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <EditProfile/>
-            <JobsPanel></JobsPanel>
-            <UnableAccount></UnableAccount>
+            <ProtectionProfile></ProtectionProfile>
         </div>
     )
 }
