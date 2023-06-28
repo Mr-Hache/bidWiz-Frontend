3623bcc29eaa95a96a1e752e9ce5dec76c9ac854
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-28 14:16:24 -0500

    ahora el componente admin solo renderiza al componente de protección de la ruta admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,18 +1,11 @@
-import Navbar from "@/app/Componets/navbar/navbar";
-import AdminDashboard from "../Componets/adminDashboard/adminDashboard";
-import styles from "./admin.module.scss";
+import ProtectionAdmin from "../Componets/ProtectionAdmin/ProtectionAdmin";
 
 export default async function admin() {
+ 
+
   return (
-    <div className={styles.adminPage}>
-      <Navbar />
-      <br />
-      <div>
-        <div className={styles.title}>
-          <h1>Dashboard Admin</h1>
-        </div>
-        <AdminDashboard />
-      </div>
+    <div>
+      <ProtectionAdmin />
     </div>
   );
 }
