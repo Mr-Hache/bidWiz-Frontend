4408e804dad8c47b8f3a21eb6a60fa3dcc8474d2
Mr-Hache <hernancastillo@unicauca.edu.co>
2023-06-28 14:08:55 -0500

    el login por Gmail está funcional, se quitó el login por githun, se bloqueó el ingreso a los usuarios que estén deshabilitados, se colocó un span para que redirija al cambio de contraseña por medio de email

--- app/Componets/auth/page.tsx
+++ app/Componets/auth/page.tsx
@@ -1,92 +1,120 @@
-"use client"
-import React, { useState, useEffect } from 'react'
-import style from "./auth.module.scss"
-import { useRouter } from 'next/navigation'
-import { auth, userSignOut } from '../../utils/firebase'  
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider,signInWithPopup, browserLocalPersistence, setPersistence } from 'firebase/auth'
+"use client";
+import React, { useState, useEffect } from "react";
+import style from "./auth.module.scss";
+import { useRouter } from "next/navigation";
+import { auth, userSignOut, loginWithGoogle } from "../../utils/firebase";
+import { 
+  signInWithEmailAndPassword,
+  browserLocalPersistence,
+  setPersistence,
+} from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
-import { useCreateUserMutation } from "@/app/redux/services/userApi"
-
+import { useCreateUserMutation } from "@/app/redux/services/userApi";
 
 function authent() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [idAuth, setIdAuth] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const router = useRouter();
   const [createUser, { data, error }] = useCreateUserMutation();
 
-
-useEffect(() =>{
-    console.log(idAuth);
-},[idAuth])
-
-  const handleSubmit = async (event :React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-   
-    setPersistence (auth, browserLocalPersistence).then(() => {
-      signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-        const user = userCredential.user;
-        setIdAuth(user.uid)
-        if(!user.emailVerified) {
-          router.push("/login")
-          userSignOut()
-          alert("email not verified")
 
-        }else{
-          console.log("usuario autenticado")
-          router.push("/offerBoard")
-        }
-      }) .catch((error) => {
-        alert("Unregistered user or incorrect password")
-          console.log(error)
-      })
-    }) .catch((error) => {
-      console.log(error)
-    })
+    setPersistence(auth, browserLocalPersistence)
+      .then(() => {
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            const user = userCredential.user;
 
-  }
-
-  const handleGoogleSignIn = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    const googleProvider = new GoogleAuthProvider();
-    try {
-      const response = await fetch('https://bidwiz-backend-production-db77.up.railway.app/users/emails');
-        const data = await response.json();
-        const userCredential = await signInWithPopup(auth, googleProvider);
-        const user = userCredential.user;
-      
-        const result = data.includes(user.email);
-
-        if(result){
-            alert("el usuario ya tiene una cuenta asociada")
-        }else{
-        createUser({
-            name: user.displayName ? user.displayName : "",
-            email: user.email ? user.email : "",
-            uidFireBase: user.uid ? user.uid : "",
-            isWizard: false,
+            if (!user.emailVerified) {
+              router.push("/login");
+              userSignOut();
+              alert("email not verified");
+            } else {
+              console.log("usuario autenticado");
+              router.push("/offerBoard");
+            }
+          })
+          .catch((error) => {
+            alert("Unregistered user or incorrect password");
+            console.log(error);
           });
-  
-          alert("creando usuario")      
-        }
-      
-
-
-    } catch (error) {
-      console.log(error);
-    }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
+  const handleGoogleSignIn = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    setPersistence(auth, browserLocalPersistence)
+      .then(() => {
+        loginWithGoogle()
+          .then((result) => {
+            const user = result.user;
 
-  
+            fetch(
+              `https://bidwiz-backend-production-db77.up.railway.app/users/emails`
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                const userEmail = data.map(
+                  (item: { email: string; isDisabled: boolean }) => item.email
+                );
+                if (userEmail.includes(user.email)) {
+                  data.forEach((item: { email: string; isDisabled: boolean }) => {
+                    if (item.email === user.email && item.isDisabled === true) {
+                    
+                      alert("user disabled");
+                     
+                      userSignOut();
+                    } else {
+                      console.log("usuario solo autenticado");
+                      router.push("/offerBoard");
+                    }
+                  });
+                } else {
+                  createUser({
+                    email: user.email ? user.email : "",
+                    name: user.displayName ? user.displayName : "",
+                    uidFireBase: user.uid,
+                    isWizard: false,
+                  });
+                  console.log("usuario creado y autenticado");
+                  router.push("/offerBoard");
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChangePassword = () =>{
+    router.push("/changePassword");
+  }
+
 
   return (
     <form className={style.form} onSubmit={handleSubmit}>
       <div className={style.container}>
-        <label htmlFor="email" className={style.label}>Email</label><br />
-        <input 
-          type="text" 
-          name="email"  
+        <label htmlFor="email" className={style.label}>
+          Email
+        </label>
+        <br />
+        <input
+          type="text"
+          name="email"
           className={style.input}
           value={email}
           onChange={(event) => setEmail(event.target.value)}
@@ -94,24 +122,32 @@ useEffect(() =>{
       </div>
       <br />
       <div className={style.container}>
-        <label htmlFor="password" className={style.label}>Password</label><br />
-        <input 
-          type="password" 
-          name="password" 
+        <label htmlFor="password" className={style.label}>
+          Password
+        </label>
+        <br />
+        <input
+          type="password"
+          name="password"
           className={style.input}
           value={password}
           onChange={(event) => setPassword(event.target.value)}
         />
       </div>
-      <button type="submit" className={style.button}>Login</button>
+      <button type="submit" className={style.button}>
+        Login
+      </button>
       <div>
-          <FcGoogle className={style.icon} />
-          <button onClick={handleGoogleSignIn}>
-            Register with Google
-          </button>
+        <div>
+          <span onClick={handleChangePassword}>
+          I have forgotten my password
+          </span>
         </div>
+        <FcGoogle className={style.icon} />
+        <button onClick={handleGoogleSignIn}>Log In with Google</button>
+      </div>
     </form>
-  )
+  );
 }
 
-export default authent
+export default authent;
