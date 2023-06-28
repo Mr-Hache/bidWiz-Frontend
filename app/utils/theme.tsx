f47cacd9b1ef833c1626475f692a553e3edd72de
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-27 21:29:09 -0300

    Cree el archivo them.tsx que es donde voy a definir todos los colores del tema dark y light

---
+++ app/utils/theme.tsx
@@ -0,0 +1,13 @@
+const lightTheme = {
+  primaryColor: "#FFFFFF",
+  backgroundColor: "#F5F5F5",
+  // Otros colores y propiedades del tema claro
+};
+
+const darkTheme = {
+  primaryColor: "#000000",
+  backgroundColor: "#222222",
+  // Otros colores y propiedades del tema oscuro
+};
+
+export { lightTheme, darkTheme };
