970d06dc4b05d7d71cccea1c4478a175656312e4
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-29 16:08:46 -0300

    Agregué la logica del dropDown en la foto de perfil para ver las opciones del perfil

--- app/Componets/loggedIn/loggedIn.tsx
+++ app/Componets/loggedIn/loggedIn.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import styles from "./loggedin.module.scss";
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 interface LoggedInContentProps {
   userName: string;
@@ -17,26 +18,52 @@ export default function loggedIn({
   isAdmin,
   handleSignOut,
 }: LoggedInContentProps) {
+  const [open, setOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const handleDropdown = (state: boolean) => {
+    setOpen(!state);
+  };
+  const handleClickDropdown = (event: MouseEvent) => {
+    if (open && !dropdownRef.current?.contains(event.target as Node)) {
+      setOpen(false);
+    }
+  };
+
+  window.addEventListener("click", handleClickDropdown);
+
   return (
-    <>
+    <div className={styles.selectorProfile}>
       <h2>Hi, {userName.split(/[ .]/)[0]}!</h2>
-      <h3 className={styles.log}>
-        <Link href="/profile" passHref>
-          <div className={styles.profileLink}>
+
+      <div className={styles.dropdown} ref={dropdownRef}>
+        <div className={styles.dropdownToggle}>
+          <a
+            onClick={(event) => handleDropdown(open)}
+            style={{ display: "inline-block" }}
+          >
             <img src={userImage} alt={userName} width="35" height="35" />
-          </div>
-        </Link>
-      </h3>
-      {isAdmin ? (
-        <h3 className={styles.log}>
-          <Link href="/admin" passHref>
-            <div className={styles.profileLink}>este es el nuevo</div>
-          </Link>
-        </h3>
-      ) : null}
-      <h3 className={styles.log}>
-        <button onClick={handleSignOut}>Sign Out</button>
-      </h3>
-    </>
+          </a>
+        </div>
+      </div>
+      {open && (
+        <div>
+          <h3 className={styles.log}>
+            <Link href="/profile" passHref>
+              <div className={styles.profileLink}>My profile</div>
+            </Link>
+          </h3>
+          {isAdmin && (
+            <h3 className={styles.log}>
+              <Link href="/admin" passHref>
+                <div className={styles.profileLink}>Dashboard</div>
+              </Link>
+            </h3>
+          )}
+          <h3 className={styles.log}>
+            <button onClick={handleSignOut}>Sign Out</button>
+          </h3>
+        </div>
+      )}
+    </div>
   );
 }
