f8767ae8c77904a89cab49e44838f02abd11485b
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-06-29 11:14:29 -0300

    Agregué estilos al searchBar para Dark and Ligth mode

--- app/Componets/searchBar/searchBar.tsx
+++ app/Componets/searchBar/searchBar.tsx
@@ -1,60 +1,71 @@
-"use client"
+"use client";
 import styles from "./searchBar.module.scss";
-import { useState, useEffect } from 'react';
-import {useRouter} from "next/navigation"
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { useGetUsersQuery, User } from "../../redux/services/userApi";
-
-
+import { useTheme } from "next-themes";
 
 export default function searchBar() {
   const router = useRouter();
+  const { theme } = useTheme();
 
   const { data: users } = useGetUsersQuery(null);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   };
 
-  const filteredUsers = users?.filter((user: User) =>
-    user.name.toLowerCase().includes(search.toLowerCase()) && user.isWizard && !user.isDisabled
+  const filteredUsers = users?.filter(
+    (user: User) =>
+      user.name.toLowerCase().includes(search.toLowerCase()) &&
+      user.isWizard &&
+      !user.isDisabled
   );
 
   const handleUserClick = (userId: string) => {
     router.push(`/detail/${userId}`);
   };
-      
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if(filteredUsers?.length === 1) {
+    if (filteredUsers?.length === 1) {
       handleUserClick(filteredUsers[0]._id);
     }
   };
-  
-  
+
   return (
     <div className={styles.searchContainer}>
       <form onSubmit={handleSubmit}>
-      <input
-        className={styles.input}
-        type="text"
-        placeholder="Find your Wizard"
-        value={search}
-        onChange={handleSearch}
-      />
-      <button type="submit" className={styles.button} >
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Find your Wizard"
+          value={search}
+          onChange={handleSearch}
+        />
+        <button type="submit" className={styles.button}>
           <div className={styles.lupa}>🔍︎</div>
         </button>
       </form>
 
       <div className={styles.contResults}>
-      {search && filteredUsers && filteredUsers.slice(0, 3).map((user: User) => (
-        <div key={user._id} className={styles.userResult} onClick={() => handleUserClick(user._id)}>
-         {user.name}          
-        </div>
-      ))}
-      </div> 
-      </div>                    
+        {search &&
+          filteredUsers &&
+          filteredUsers.slice(0, 3).map((user: User) => (
+            <div
+              key={user._id}
+              className={`${styles.userResult} ${
+                theme === "dark"
+                  ? styles.userResultDark
+                  : styles.userResultLight
+              }`}
+              onClick={() => handleUserClick(user._id)}
+            >
+              {user.name}
+            </div>
+          ))}
+      </div>
+    </div>
   );
 }
-
