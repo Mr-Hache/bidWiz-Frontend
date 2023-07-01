7dbd910b59638b042468b860c51106ac9d91eb75
mickelsengerald <mickelsengerald@gmail.com>
2023-06-30 21:56:40 -0400

    test 3 deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,6 @@ import styles from "./layout.module.scss";
 import { Providers } from "./redux/providers";
 import { ThemeProvider } from "next-themes";
 
-export const metadata = {
-  title: "Bidwiz",
-  description: "",
-};
-
 export default function RootLayout({
   children,
 }: {
