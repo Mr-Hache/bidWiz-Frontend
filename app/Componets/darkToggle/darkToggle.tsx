f26c393f7f71d6a67bbf110405c3c90aeb06ea68
mickelsengerald <mickelsengerald@gmail.com>
2023-06-30 22:02:26 -0400

    test 4 deploy

--- app/Componets/darkToggle/darkToggle.tsx
+++ app/Componets/darkToggle/darkToggle.tsx
@@ -1,35 +1,35 @@
 
 
-import React, { useEffect, useState } from 'react';
-import { BsSunFill } from 'react-icons/bs';
-import { BsMoonFill } from 'react-icons/bs';
-import { useTheme } from 'next-themes';
-import styles from './darkToggle.module.scss';
+// import React, { useEffect, useState } from 'react';
+// import { BsSunFill } from 'react-icons/bs';
+// import { BsMoonFill } from 'react-icons/bs';
+// import { useTheme } from 'next-themes';
+// import styles from './darkToggle.module.scss';
 
-export default function DarkToggle() {
-  const [mounted, setMounted] = useState(false);
-  const { resolvedTheme, setTheme } = useTheme();
+// export default function DarkToggle() {
+//   const [mounted, setMounted] = useState(false);
+//   const { resolvedTheme, setTheme } = useTheme();
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+//   useEffect(() => {
+//     setMounted(true);
+//   }, []);
 
-  const handleToggleTheme = () => {
-    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
-  };
+//   const handleToggleTheme = () => {
+//     setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+//   };
 
-  // No render the component until the component is mounted
-  if (!mounted) return null;
+//   // No render the component until the component is mounted
+//   if (!mounted) return null;
 
-  return (
-    <button className={styles.button} onClick={handleToggleTheme}>
-      {resolvedTheme === 'dark' ? (
-        <BsSunFill className={styles.sun} title="Light Mode" />
-      ) : (
-        <BsMoonFill className={styles.moon} title="Dark Mode" />
-      )}
-    </button>
-  );
-}
+//   return (
+//     <button className={styles.button} onClick={handleToggleTheme}>
+//       {resolvedTheme === 'dark' ? (
+//         <BsSunFill className={styles.sun} title="Light Mode" />
+//       ) : (
+//         <BsMoonFill className={styles.moon} title="Dark Mode" />
+//       )}
+//     </button>
+//   );
+// }
 
 
