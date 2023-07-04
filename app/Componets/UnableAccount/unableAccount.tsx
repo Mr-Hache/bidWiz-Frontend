2413d35940f3ecefdd436101055e2d29f5ae1929
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-07-04 12:57:11 -0300

    Cambié el boton del unable

--- app/Componets/UnableAccount/unableAccount.tsx
+++ app/Componets/UnableAccount/unableAccount.tsx
@@ -1,62 +1,69 @@
-"use client"
+"use client";
 
-import { useDisableUserMutation } from '../../redux/services/userApi';
-import {useAppSelector} from "../../redux/hooks"
-import { useState, useEffect } from 'react';
-import styles from "./unableAccount.module.scss"
+import { useDisableUserMutation } from "../../redux/services/userApi";
+import { useAppSelector } from "../../redux/hooks";
+import { useState, useEffect } from "react";
+import styles from "./unableAccount.module.scss";
 import { usePathname, useRouter } from "next/navigation";
 import { userSignOut } from "../../utils/firebase";
 
 export default function UnableAccount() {
-  const localUid = useAppSelector((state) => state.userAuth.uid)
+  const localUid = useAppSelector((state) => state.userAuth.uid);
   const [disableUserMutation] = useDisableUserMutation();
-  const [userId, setUserId] = useState('');
-  const pathname = usePathname()
-  const router = useRouter()
+  const [userId, setUserId] = useState("");
+  const pathname = usePathname();
+  const router = useRouter();
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   const fetchUserData = () => {
-    fetch(`https://bidwiz-backend-production-db77.up.railway.app/users/user/${localUid}`)
-    .then(response => response.json())
-    .then(data => { setUserId(data._id)})
-    .catch(error => console.error(error));
-}
+    fetch(
+      `https://bidwiz-backend-production-db77.up.railway.app/users/user/${localUid}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setUserId(data._id);
+      })
+      .catch((error) => console.error(error));
+  };
 
-useEffect(() => {
+  useEffect(() => {
     fetchUserData();
-    const disable = true  
-}, [localUid]);
+    const disable = true;
+  }, [localUid]);
 
-
-const handleClick = async () => {
-  if (showConfirmation) {
-    try {
-      await disableUserMutation({ _id: userId });
-      router.push("/");
-      await userSignOut();
-    } catch (error) {
-      console.error(error);
+  const handleClick = async () => {
+    if (showConfirmation) {
+      try {
+        await disableUserMutation({ _id: userId });
+        router.push("/");
+        await userSignOut();
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      setShowConfirmation(true); // Al hacer clic en "Unable Account", se muestra la pregunta de confirmación
     }
-  } else {
-    setShowConfirmation(true); // Al hacer clic en "Unable Account", se muestra la pregunta de confirmación
-  }
-};
+  };
 
-const handleCancel = () => {
-  setShowConfirmation(false); // Al hacer clic en "Cancel", se oculta la pregunta de confirmación
-};
+  const handleCancel = () => {
+    setShowConfirmation(false); // Al hacer clic en "Cancel", se oculta la pregunta de confirmación
+  };
 
-return (
-  <div className={styles.div}>
-    {showConfirmation ? (
-      <>
-        <label>Are You Sure?</label>
-        <button onClick={handleClick}>Yes</button>
-        <button onClick={handleCancel}>Cancel</button>
-      </>
-    ) : (
-      <button onClick={handleClick}>Unable Account</button>
-    )}
-  </div>
-);
+  return (
+    <div className={styles.div}>
+      {showConfirmation ? (
+        <>
+          <label>Are You Sure?</label>
+          <div>
+            <button onClick={handleClick}>Yes</button>
+            <button className={styles.cancel} onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        </>
+      ) : (
+        <button onClick={handleClick}>Unable Account</button>
+      )}
+    </div>
+  );
 }
